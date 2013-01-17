d66ff4c97e2772f80edec83b891209b56ca7ba5d
Kaiwei <iamkaiwei@gmail.com>
2013-01-17 16:10:02 +0700

    add

---
+++ test/Calculator.h
@@ -0,0 +1,13 @@
+//
+//  Calculator.h
+//  test
+//
+//  Created by User on 1/17/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Calculator : NSObject
+
+@end
---
+++ test/Calculator.m
@@ -0,0 +1,13 @@
+//
+//  Calculator.m
+//  test
+//
+//  Created by User on 1/17/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import "Calculator.h"
+
+@implementation Calculator
+
+@end
