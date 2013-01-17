fa64aef1f8a946c8288857869b18921625c92251
Kaiwei <iamkaiwei@gmail.com>
2013-01-17 16:05:04 +0700

    initial commit

---
+++ test/CalculatorAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CalculatorAppDelegate.h
+//  test
+//
+//  Created by User on 1/17/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ test/CalculatorViewController.m
@@ -0,0 +1,34 @@
+//
+//  CalculatorViewController.m
+//  test
+//
+//  Created by User on 1/17/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import "CalculatorViewController.h"
+
+@interface CalculatorViewController ()
+
+@end
+
+@implementation CalculatorViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
